2d842de508e795cc41e26312fd750a5ce836b8c1
sato884 <sergiomfernandos@gmail.com>
2026-09-13 11:07:59 -0300

    Update analisis_steam.sql

--- hive/analisis_steam.sql
+++ hive/analisis_steam.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS steam_analytics;
 USE steam_analytics;
 
--- Tabla externa Schema-on-Read apuntando a la salida de Pig
-CREATE EXTERNAL TABLE IF NOT EXISTS steam_games_processed (
+DROP TABLE IF EXISTS steam_games_processed;
+
+CREATE EXTERNAL TABLE steam_games_processed (
     appid STRING,
     name STRING,
-    price FLOAT,
+    price DOUBLE,
     is_free_to_play INT,
     dlc_count INT,
     achievements INT,
@@ -23,11 +24,9 @@ FIELDS TERMINATED BY '\t'
 STORED AS TEXTFILE
 LOCATION '/user/cloudera/proyecto/processed/juegos_etiquetados';
 
--- Consulta analitica de negocio
 INSERT OVERWRITE DIRECTORY '/user/cloudera/proyecto/output/top_satisfaccion'
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
 SELECT name, price, satisfaction_ratio, peak_ccu
 FROM steam_games_processed
-WHERE (positive + negative) > 0
 ORDER BY satisfaction_ratio DESC
 LIMIT 10;
