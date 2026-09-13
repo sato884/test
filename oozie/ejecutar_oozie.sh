5f54a55651112a05979c2d153fbe9adb7213cd29
sato884 <sergiomfernandos@gmail.com>
2026-09-12 21:48:37 -0300

    Update and rename ejecutar_oozie.sh to ejecutar_oozie.sh

--- oozie/ejecutar_oozie.sh
+++ oozie/ejecutar_oozie.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+# 1. Crear estructura de carpetas en HDFS
 hdfs dfs -mkdir -p /user/cloudera/proyecto/raw/games_sqoop
 hdfs dfs -mkdir -p /user/cloudera/proyecto/processed
 hdfs dfs -mkdir -p /user/cloudera/proyecto/output
 hdfs dfs -mkdir -p /user/cloudera/proyecto/scripts
 hdfs dfs -mkdir -p /user/cloudera/proyecto/oozie
 
+# 2. Cargar dataset crudo a HDFS
 hdfs dfs -put -f /home/cloudera/workspace/test/pig/dataset_final_hive.tsv /user/cloudera/proyecto/raw/games_sqoop/part-m-00000
+
+# 3. Subir scripts ejecutables y definicion a HDFS
 hdfs dfs -put -f /home/cloudera/workspace/test/pig/etl_steam.pig /user/cloudera/proyecto/scripts/
 hdfs dfs -put -f /home/cloudera/workspace/test/hive/analisis_steam.sql /user/cloudera/proyecto/scripts/
 hdfs dfs -put -f /home/cloudera/workspace/test/oozie/workflow.xml /user/cloudera/proyecto/oozie/
 
+# 4. Limpiar carpetas de salida previas
 hdfs dfs -rm -r /user/cloudera/proyecto/processed/juegos_etiquetados 2>/dev/null || true
 hdfs dfs -rm -r /user/cloudera/proyecto/output/top_satisfaccion 2>/dev/null || true
 
+# 5. Ejecutar workflow en Apache Oozie
 echo "Lanzando Workflow en Oozie..."
 oozie job -oozie http://localhost:11000/oozie -config job.properties -run
